e4eb14349074bc8008c404de53d3985150c07b8b
Yiyang Yu <s4875800@student.uq.edu.au>
2025-01-06 16:42:19 +0800

    Add
    BBox added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import cv2
 import argparse
 from ultralytics import YOLO
+import supervision as sv
 
 def parse_args():
     parser = argparse.ArgumentParser(description='YOLOv8 live')
@@ -17,17 +18,27 @@ def main():
     args = parse_args()
     frame_width, frame_height = args.webcam_resolution
 
-    cap = cv2.VideoCapture(1)
+    cap = cv2.VideoCapture(0)
     cap.set(cv2.CAP_PROP_FRAME_WIDTH, frame_width)
     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, frame_height)
 
     model = YOLO('yolov8l.pt')
 
+    box_annotator = sv.BoxAnnotator(
+        thickness=2,
+        text_thickness=2,
+        text_scale=1,
+    )
+
     while True:
         ret, frame = cap.read()
-        cv2.imshow('yolov8', frame)
 
-        result = model(frame)
+        result = model(frame)[0]
+        detections = sv.Detections.from_yolov8(result)
+
+        frame = box_annotator.annotate(scene=frame, detections=detections)
+
+        cv2.imshow('yolov8', frame)
 
         if (cv2.waitKey(30) == 27):
             break
